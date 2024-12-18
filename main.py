22828ae1fd9fa99685ec43fba91d4cf84007d655
jleandro92 <jleandro0802@gmail.com>
2024-12-18 17:07:19 -0300

    HP

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ def main():
     Hospital.configurar_hospital_para_sessao("Hospital João Macchado", 5)
 
     menu = Menu(7) 
-    menu.adicionar_comando(0, Sair())
     menu.adicionar_comando(1, CriarAlaComTamanhoFixo())
     menu.adicionar_comando(2, AdicionarPacienteEmUmLeitoDaAla())
     menu.adicionar_comando(3, RealocarPaciente())
     menu.adicionar_comando(4, RelatorioDeAla())
     menu.adicionar_comando(5, AdicionarExame()) 
     menu.adicionar_comando(6, RemoverExame())
+    menu.adicionar_comando(0, Sair())
 
     menu.apresentar_e_executar()
 

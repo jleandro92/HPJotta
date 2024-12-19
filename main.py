020bb509754becb04221658756d1ea5e61fe754f
jleandro92 <116379490+jleandro92@users.noreply.github.com>
2024-12-19 14:01:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-from apresentacao.comando import Sair, CriarAlaComTamanhoFixo, AdicionarPacienteEmUmLeitoDaAla, RelatorioDeAla, RealocarPaciente
+from apresentacao.comando import Sair, CriarAlaComTamanhoFixo, AdicionarPacienteEmUmLeitoDaAla, RelatorioDeAla, RealocarPaciente, RemoverPaciente
 from apresentacao.menu import Menu
 from dominio.hospital import Hospital
 from dominio.paciente import AdicionarExame, RemoverExame
 
+
 def main():
     Hospital.configurar_hospital_para_sessao("Hospital João Macchado", 5)
 
-    menu = Menu(7) 
+    menu = Menu(8) 
     menu.adicionar_comando(1, CriarAlaComTamanhoFixo())
     menu.adicionar_comando(2, AdicionarPacienteEmUmLeitoDaAla())
     menu.adicionar_comando(3, RealocarPaciente())
     menu.adicionar_comando(4, RelatorioDeAla())
     menu.adicionar_comando(5, AdicionarExame()) 
     menu.adicionar_comando(6, RemoverExame())
+    menu.adicionar_comando(7, RemoverPaciente())
     menu.adicionar_comando(0, Sair())
 
     menu.apresentar_e_executar()
